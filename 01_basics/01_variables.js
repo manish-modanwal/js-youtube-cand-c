2cc023c9228f68ad1b109278f4302677f3bc0454
manish-modanwal <152703377+manish-modanwal@users.noreply.github.com>
2024-09-05 05:24:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail="manish@google.com"
+var accountPassword= "12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2
+accountEmail= "hc@hc.com"
+/*
+prefered not to use  var 
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+// alert(3+3)  // we are using node js, not browser
+
+let name="manish"
+let age=20
+let isLogged=false
+// number 2 to power 53
+//bigint
+//string=> " "
+//boolean=> true/false
+//null  =>  standalone value
+// undefined =>
+//symbol => uniqueness
+
+
+//object
+
+console.log(typeof "manish")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
