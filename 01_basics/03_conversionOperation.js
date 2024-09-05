d2cc0a7436adcddf3ffde18853230ae4a2a52e3c
manish-modanwal <152703377+manish-modanwal@users.noreply.github.com>
2024-09-05 06:00:59 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33"
+let age=null;
+
+
+console.log(typeof(score));
+
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let valueInAge=Number(age);
+console.log(typeof valueInAge)
+console.log(valueInAge)
+
+
+
+
+//"33" => 33
+//"33abc" => NaN
+// null => 0
+// true => 1 ,false =>0;
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1=> true
+//""=> false
+// "manish"=> true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber)
