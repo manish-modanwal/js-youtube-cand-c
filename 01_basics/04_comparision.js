f29dd969bfe769d5f7c99bbe82ef26c201e41a1e
manish-modanwal <152703377+manish-modanwal@users.noreply.github.com>
2024-09-15 09:39:28 +0000

    data type summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//the comparision > < >= <= work diffirently first it convert null to a number that is 0
+//thats why  null>=0 is true and  null>0 is false
+
+
+
+//=== strict check
+
+console.log("2"===2);
+
